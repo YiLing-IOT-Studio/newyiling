3a380cb56ab8be905366a852323b38e6923730ff
langmo <1026052783@qq.com>
2020-10-16 19:47:10 +0800

    第一次合并杨的代码

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueParticles from 'vue-particles'
+import ElementUI from 'element-ui'
+
 import './assets/icon/iconfont.css'
 import './assets/icon2/iconfont.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueFullpage from 'vue-fullpage.js'
+
+Vue.use(VueFullpage)
 Vue.use(VueParticles)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import aboutUs from '@/components/aboutUs/aboutUs'
 import joinUs from '@/components/joinUs/joinUs'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home/home'
+import Index from '@/components/index'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index,
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/aboutus',
       name: 'aboutUs',
@@ -16,11 +28,6 @@ export default new Router({
       path: '/joinus',
       name: 'joinus',
       component: joinUs
-    },
-    {
-      path: '/',
-      name: 'hellowrold',
-      component: HelloWorld
     }
   ]
 })
